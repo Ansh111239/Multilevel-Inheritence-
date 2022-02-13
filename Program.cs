27fe223d8f364a56f111fac75ac94f2673c8a3f1
Ansh111239 <99594251+Ansh111239@users.noreply.github.com>
2022-02-13 18:04:22 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp1
+{
+
+    
+    public class Car
+    {
+        public Car()
+        {
+            Console.WriteLine("Class: Car");
+        }
+
+        public void vehicleType()
+        {
+            
+            Console.WriteLine("Vehicle Type: Car");
+        }
+    }
+    public class Honda : Car
+    {
+        public Honda()
+        {
+            Console.WriteLine("Class: Honda");
+        }
+        public void brand()
+        {
+            Console.WriteLine("Brand: Honda");
+        }
+        public void speed()
+        {
+            Console.WriteLine("Max: 120Kmph");
+        }
+        
+
+    }
+
+    public class Civic : Honda
+    {
+        public Civic()
+        {
+            Console.WriteLine("Model: Civic");
+        }
+
+        public void speed()
+        {
+            Console.WriteLine("Max: 150Kmph");
+        }
+
+        /*
+        public void working(string z)
+        {
+            
+            Console.WriteLine("Is the car in working condition?");
+            z = Console.ReadLine();
+            Console.WriteLine(z);
+        }
+        */
+    }
+
+
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Civic obj = new Civic();
+            obj.vehicleType();
+            obj.brand();
+            obj.speed();
+            
+            
+            //obj.working();   
+            Console.ReadLine();
+
+        }
+    }
+}
